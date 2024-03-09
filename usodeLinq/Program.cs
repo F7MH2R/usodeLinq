fa1fb0f6058246425a8879368f9a66a2f6113085
F7MH2R <skyicef7@gmail.com>
2024-03-08 23:22:41 -0600

    SI

--- usodeLinq/Program.cs
+++ usodeLinq/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using usodeLinq.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+//Inyeccion de datos 
+builder.Services.AddDbContext<EquiposContexts>(
+    options=> options.UseSqlServer(
+        builder.Configuration.GetConnectionString(
+            "LINQequiposDbConnection")
+        )
+    );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
