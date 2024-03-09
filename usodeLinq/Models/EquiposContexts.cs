425be75da2ffe2ab3c3145050cd1e6970b7433c8
F7MH2R <skyicef7@gmail.com>
2024-03-08 23:23:00 -0600

    SI

--- usodeLinq/Models/EquiposContexts.cs
+++ usodeLinq/Models/EquiposContexts.cs
@@ -1,6 +1,18 @@
-﻿namespace usodeLinq.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace usodeLinq.Models
 {
-    public class EquiposContexts
+    public class EquiposContexts: DbContext
     {
+        public EquiposContexts(DbContextOptions<EquiposContexts>options):base (options) 
+        { 
+        
+        }
+
+        public DbSet<Equipos> equipos { get; set; }
+        public DbSet<estadoEquipo> estados_equipo { get; set; }
+        public DbSet<marcas> marcas { get; set; }
+        public DbSet<TipoEquipo> tipo_equipo { get; set; }
+ 
     }
 }
