ac35301c5ff4632248b04bfba2d6c02cba346660
F7MH2R <skyicef7@gmail.com>
2024-03-09 00:12:18 -0600

    si

--- usodeLinq/Controllers/EquiposController.cs
+++ usodeLinq/Controllers/EquiposController.cs
@@ -9,6 +9,7 @@ namespace usodeLinq.Controllers
     public class EquiposController : ControllerBase
     {
         private readonly EquiposContexts _equiposContext;
+     
 
         public EquiposController(EquiposContexts equiposContext)
         {
@@ -25,14 +26,25 @@ namespace usodeLinq.Controllers
 
         public IActionResult Get()
         {
-            List<Equipos> listadoequipo = (from e in _equiposContext.equipos
+            var listadoequipo = (from e in _equiposContext.equipos
                                            join t in _equiposContext.tipo_equipo
                                            on e.tipo_equipo_id equals t.id_tipo_equipo
                                            join m in _equiposContext.marcas
                                            on   e.marca_id equals m.id_marcas
                                            join es in _equiposContext.estados_equipo
                                            on e.estado_equipo_id equals es.id_estados_equipo
-                                           select e).ToList();
+                                           select new
+                                           {
+                                               e.id_equipos,
+                                               e.nombre,
+                                               e.descripcion,
+                                               e.tipo_equipo_id,
+                                               tipo_equipo= t.descripcion,
+                                               e.marca_id,
+                                               marca=m.nombre_marca,
+                                               e.estado_equipo_id,
+                                               estado_equipo=es.descripcion,
+                                           }).ToList();
 
             if (listadoequipo.Count == 0)
             {
