955b9514f6e17ae8a4e209450bf3578a608fde77
F7MH2R <skyicef7@gmail.com>
2024-03-08 23:07:15 -0600

    si

--- usodeLinq/Models/TipoEquipo.cs
+++ usodeLinq/Models/TipoEquipo.cs
@@ -1,6 +1,12 @@
-﻿namespace usodeLinq.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace usodeLinq.Models
 {
     public class TipoEquipo
     {
+        [Key]
+        public int id_tipo_equipo { get; set; }
+        public string descripcion { get; set; }
+        public string estado { get; set; }
     }
 }
